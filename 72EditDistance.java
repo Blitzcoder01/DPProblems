10ddbad4dc7fb87b8acfa582a35a96cb13f0da1f
Riya Mathur <51375575+Blitzcoder01@users.noreply.github.com>
2021-10-01 20:28:37 +0530

    Create 72EditDistance.java

---
+++ 72EditDistance.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minDistance(String word1, String word2) {
+        int[][] dp =new int[word2.length()+1][word1.length()+1];
+        for(int i=0; i<word2.length()+1; i++){
+            for(int j=0; j<word1.length()+1; j++){
+                if(i==0 || j==0){
+                    dp[i][j]=j;
+                }
+                else if(word2.charAt(i-1)== word1.charAt(j-1)){
+                    dp[i][j]= dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j]= 1+Math.min(dp[i-1][j],Math.min(dp[i-1][j-1], dp[i][j-1]));
+                }
+            }
+        }
+        return dp[word2.length()][word1.length()];
+    }
+}
